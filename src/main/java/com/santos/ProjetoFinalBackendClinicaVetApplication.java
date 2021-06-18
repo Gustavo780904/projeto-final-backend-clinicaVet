73155743a838ada89533e6092da477c30210fb60
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-06-18 19:46:32 -0300

    first commit

---
+++ src/main/java/com/santos/ProjetoFinalBackendClinicaVetApplication.java
@@ -0,0 +1,13 @@
+package com.santos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBackendClinicaVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBackendClinicaVetApplication.class, args);
+	}
+
+}
